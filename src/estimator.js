5e3d26195321a6d3c12ade129b18e362292dc7f6
Tony <tonykipsang7@gmail.com>
2020-04-12 14:49:54 +0300

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,24 +1,20 @@
 const covid19ImpactEstimator = (data) => data;
-  
+
+const currentlyInfected = reportedCases * 10;
+
 
 
 export default covid19ImpactEstimator;
 
-function impact (){
-    this.currentlyInfected = reportedCases * 10;
 
+
+function impact(){
+    const currentlyInfected = reportedCases * 10;
 }
-
-
-//severely impacted
-function severeImpact (){
-    this.currentlyInfected = reportedCases * 50;
+function severeImpact(){
+    const currentlyInfected = reportedCases * 50;
 }
-
-//Within 30 days
-function infectionsByRequestedTime (){
+function infectionsByRequestedTime(){
     severeImpact.currentlyInfected = reportedCases * 1024;
     impact.currentlyInfected *  1024;
-   
-
 }
