50cb4df385586d7f219ed5a0834f6e6179accfe4
Tony <tonykipsang7@gmail.com>
2020-04-14 07:34:40 +0300

    fix errors

--- src/estimator.js
+++ src/estimator.js
@@ -1,22 +1,22 @@
-const covid19ImpactEstimator = (data) => data;
-const reportedCases = {};
-impact.currentlyInfected = reportedCases * 10;
-severeImpact.currentlyInfected = reportedCases * 10;
-
-
-function impact() {
-  
+const covid19ImpactEstimator = (data) => {
+const input = data;
+const currentlyInfected = data.reportedCases * 10;
+const severeCurrentlyInfected = data.reportedCases * 50;
 }
-impact(currentlyInfected);
 
-function severeImpact() {
-  
+// Estimate
+let estimateTime;
+if (data.periodType === 'days') {
+    estimateTime = data.timeToElapse;
+}else if (data.periodType === 'weeks') {
+    estimateTime = data.timeToElapse * 7;
+}else if (data.periodType === 'months'){
+    estimateTime = data.timeToElapse * 30;
 }
-severeImpact(currentlyInfected);
+const setOfDays = Math.floor(estimateTime / 3);
+const infectionsByRequestedTime = currentlyInfected * (2 ** setOfDays);
+const severeInfectionsByRequestedTime = severeCurrentlyInfected * (2 ** setOfDays);
 
-function infectionsByRequestedTime() {
-  Math.trunc(impact.currentlyInfected * 512);
-  Math.trunc(severeImpact.currentlyInfected * 512);
-}
-infectionsByRequestedTime();
+const severeCasesByRequestedTime = Math.floor((15 /100) * infectionsByRequestedTime);
+
 export default covid19ImpactEstimator;
